fe0b1837044c8bdb1bf0ba2fbe14dc6ec97f7e27
Tino Taipale <tino.taipale@arnon.fi>
2023-03-13 12:26:17 +0200

    initialize repo

---
+++ telegraf-installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##########################################################################  
+# Script Name    : Telegraf Installer                                                                                               
+# Description    : Download and install Telegraf natively on Debian/Ubuntu
+# Creation Date  : 2023/20/13                                                   
+# Author         :                                             
+# Email          :                                           
+##########################################################################
