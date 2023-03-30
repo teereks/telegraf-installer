db20a9533d7d5327ab65f4b83e9cd139471ea03e
teereks <47917519+teereks@users.noreply.github.com>
2023-03-30 15:55:18 +0300

    update dialogs and logging

--- telegraf-installer.sh
+++ telegraf-installer.sh
@@ -47,38 +47,39 @@ function remove_influxdata_upstream() {
     # Array of files to be deleted
     local files=("$INFLUXDATA_KEYPATH" "$INFLUXDATA_SRCPATH")
 
-    if (whiptail --title "Warning" --yesno "Removing current InfluxData-repository source and GPG-key will disable Telegraf-updates from upstream.\nOther InfluxData-products might be affected as well, if they use the same sources. The following files will be deleted:\n--> $INFLUXDATA_SRCPATH\n--> $INFLUXDATA_KEYPATH\n\nDo you want to remove these files?" 16 78); then
-        echo "User selected Yes, exit status was $?."
+    if (whiptail --title "Warning" --yesno "Removing current InfluxData-repository source and GPG-key will disable Telegraf-updates from upstream.\nOther InfluxData-products might be affected as well, if they use the same sources. The following files will be deleted:\n--> $INFLUXDATA_SRCPATH\n--> $INFLUXDATA_KEYPATH\n\nDo you want to remove these files?" 16 78 --no-button "Return"); then
+        echo "[influx-upstream-remove]: User selected Yes, exit status was $?."
         # Iterate files, delete if it exists
         for file in "${files[@]}"; do
             if [ -f "$file" ]; then
                 rm "$file"
             fi
         done
+        whiptail --title "InfluxData-repository" --msgbox "InfluxData-repository source and/or GPG-key deleted." 18 78
     else
-        echo "User selected No, exit status was $?."
-        return
+        echo "[influx-upstream-remove]: User selected No, exit status was $?."
+        return 1
     fi
 }
 
 # Add InfluxData related GPG-keys and listed sources to system
 function add_influxdata_upstream() {
-    TITEMPDIR=$(whiptail --inputbox "Select temporary file-path to store InfluxData GPG-key:" 10 78 /tmp/telegraf-installer --title "Temporary file-path" 3>&1 1>&2 2>&3)
+    TITEMPDIR=$(whiptail --inputbox "Select temporary file-path to store InfluxData GPG-key:" 10 78 /tmp/telegraf-installer --title "Temporary file-path" --cancel-button "Return" 3>&1 1>&2 2>&3)
     exitstatus=$?
     if [ $exitstatus = 0 ]; then
-        echo "User selected Ok and entered " $TITEMPDIR
+        echo "[influx-upstream (path)]: User selected Ok and entered " $TITEMPDIR
     else
-        echo "User selected Cancel."
-        return
+        echo "[influx-upstream (path)]: User selected Cancel."
+        return 1
     fi
 
     # Check if temporary directory already exists
     if [[ -d $TITEMPDIR ]]; then
-        if (whiptail --title "Warning" --yesno "$TITEMPDIR already exists and continuing might overwrite files located in that directory.\n\nDo you want to continue?" 12 78); then
-            echo "User selected Yes, exit status was $?."
+        if (whiptail --title "Warning" --yesno "$TITEMPDIR already exists and continuing might overwrite files located in that directory.\n\nDo you want to continue?" 12 78 --no-button "Return"); then
+            echo "[influx-upstream-add (overwrite)]: User selected Yes, exit status was $?."
         else
-            echo "User selected No, exit status was $?."
-            return
+            echo "[influx-upstream-add (overwrite)]: User selected No, exit status was $?."
+            return 1
         fi
     fi
 
@@ -101,6 +102,8 @@ function add_influxdata_upstream() {
     # Add Influxdata-repo as a package source
     echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
     cd -
+
+    whiptail --title "InfluxData-repository" --msgbox "Repository added to sources." 18 78
 }
 
 # Check status of given options on this machine
@@ -118,7 +121,10 @@ function installeroptions() {
 
 # Present given options
 function selectoptions() {
-    result=$(whiptail --title "$title" --checklist "$text" 22 78 8 "${choices[@]}" 3>&2 2>&1 1>&3-)
+    result=$(whiptail --title "$title" --checklist "$text" 22 78 8 --cancel-button "Exit" "${choices[@]}" 3>&2 2>&1 1>&3-)
+    exitstatus=$?
+    [[ $exitstatus = 1 ]] && installerexit
+    return 0
 }
 
 # Install selected options
@@ -127,23 +133,21 @@ function exitorinstall() {
     if [ $exitstatus = 0 ]; then
         programs=$(echo $result | sed 's/" /\n/g' | sed 's/"//g')
         echo $programs
-        if [[ "${programs[*]}" =~ "telegraf" ]]; then
-            telegrafwarning
-        fi
+        [[ "${programs[*]}" =~ "telegraf" ]] && telegrafwarning
         apt-get -y --ignore-missing install $programs || echo "ERROR: Installation failed. Could not install selected programs." >&2
     else
-        echo "User selected Cancel."
+        echo "[package-options]: User selected Cancel."
     fi
 }
 
 # Present warning for changing Telegraf-files
 function telegrafwarning() {
-    if (whiptail --title "Telegraf - Warning" --yesno "You selected Telegraf to be installed OR it is already installed on this machine. Continuing might have effect on Telegraf and Telegraf-related files on this system if they already exist.\n\nDo you want to continue?" 12 78); then
-        echo "User selected Yes, exit status was $?."
+    if (whiptail --title "Telegraf - Warning" --yesno "You selected Telegraf to be installed OR it is already installed on this machine. Continuing might have effect on Telegraf and Telegraf-related files on this system if they already exist.\n\nDo you want to continue?" 12 78 --no-button "Exit"); then
+        echo "[telegraf-warning]: User selected Yes, exit status was $?."
         telegrafmenu
     else
-        echo "User selected No, exit status was $?."
-        exit 1
+        echo "[telegraf-warning]: User selected Exit, exit status was $?."
+        installerexit
     fi
 }
 
@@ -152,7 +156,7 @@ function definehold() {
     local holdresult="N/A"
     while [ 1 ]; do
         HOLDCHOICE=$(
-            whiptail --title "Hold/Unhold - $1" --menu "Select 'Finish' to return to previous menu." 14 78 6 \
+            whiptail --title "Hold/Unhold - $1" --menu "Select 'Finish' to return to previous menu." 14 78 6 --cancel-button "Return" \
                 "1)" "<-- Return to previous menu" \
                 "2)" "Hold $1" \
                 "3)" "Unhold $1" 3>&2 2>&1 1>&3
@@ -160,7 +164,7 @@ function definehold() {
         exitstatus=$?
         [[ $exitstatus = 1 ]] && return
 
-        echo "choise is: $HOLDCHOICE"
+        echo "[hold-menu]: User selected: $HOLDCHOICE"
         case $HOLDCHOICE in
         "1)")
             return
@@ -182,7 +186,7 @@ function definehold() {
 function telegrafmenu() {
     while [ 1 ]; do
         CHOICE=$(
-            whiptail --title "Telegraf Operations" --menu "Select 'Finish' to continue installing previously selected programs." 14 78 6 \
+            whiptail --title "Telegraf Operations" --menu "Select 'Finish' to continue installing previously selected programs." 14 78 6 --cancel-button "Exit" \
                 "1)" "Check Telegraf information" \
                 "2)" "Hold/Unhold updates" \
                 "3)" "Add InfluxData repository as source" \
@@ -190,9 +194,9 @@ function telegrafmenu() {
                 "5)" "Finish" 3>&2 2>&1 1>&3
         )
         exitstatus=$?
-        [[ $exitstatus = 1 ]] && exit 1
+        [[ $exitstatus = 1 ]] && installerexit
 
-        echo "choise is: $CHOISE"
+        echo "[telegraf-menu]: User selected: $CHOICE"
         case $CHOICE in
         "1)")
             telegrafinfo
@@ -240,7 +244,7 @@ function telegrafinfo() {
         keypath="Found: /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg"
     fi
 
-    whiptail --title "Telegraf Information" --msgbox "Current Telegraf version:\n-->$telegrafversion \n\nTelegraf - apt-mark status:\n-->$holdstatus\n\nInfluxData-repository as source:\n-->$sourcepath \n\nInfluxData GPG-key:\n-->$keypath" 18 78
+    whiptail --title "Telegraf Information" --msgbox "Current Telegraf version:\n-->$telegrafversion \n\nTelegraf - apt-mark status:\n-->$holdstatus\n\nInfluxData-repository as source:\n-->$sourcepath \n\nInfluxData GPG-key:\n-->$keypath" 18 78 --ok-button "Return"
 }
 
 # Present programs to install
@@ -256,31 +260,40 @@ function packages() {
     installeroptions && selectoptions && exitorinstall
 }
 
-verify_root
-
-echo "Checking prerequisites..."
-check_command_availability "${PREREQUISITES[@]}"
-if [ $? -ne 0 ]; then
-    echo "Could not find the required commands on the system, trying to install missing packages."
-    install_packages "${PREREQUISITES[@]}"
+# Check dependencies
+function precheck() {
+    echo "[precheck]: Checking prerequisites..."
     check_command_availability "${PREREQUISITES[@]}"
-    if [ $? -ne 0 ]; then echo "ERROR: Failed to install prerequisites." >&2 && exit 1; fi
-    echo "Prerequisites installed succesfully!"
-else
-    echo "All prerequisites found!"
-fi
+    if [ $? -ne 0 ]; then
+        echo "[precheck]: Could not find the required commands on the system, trying to install missing packages."
+        install_packages "${PREREQUISITES[@]}"
+        check_command_availability "${PREREQUISITES[@]}"
+        if [ $? -ne 0 ]; then echo "ERROR: Failed to install prerequisites." >&2 && exit 1; fi
+        echo "[precheck]:Prerequisites installed succesfully!"
+    else
+        echo "[precheck]: All prerequisites found!"
+    fi
+    return 0
+}
+
+function installerexit() {
+    whiptail --title "Exit" --msgbox "You have exited the installer. Click OK to close this window." 8 78 && exit 1
+}
+
+verify_root
+precheck
 
 whiptail --title "Telegraf-Installer" --msgbox "Welcome to Telegraf-Installer. Select OK to continue." 10 78
 
 if (whiptail --title "Telegraf-Installer" --yesno "This script is mainly used to install Telegraf and other related packages on this machine. Installing packages using this script requires working Internet-connection, so make sure to verify this before advancing the installer.\n\nDo you want to continue?" 12 78 --no-button "Exit" --yes-button "Continue"); then
-    echo "User selected Continue, exit status was $?."
+    echo "[verify-inet]: User selected Continue, exit status was $?."
 else
-    echo "User selected Exit, exit status was $?."
-    exit 0
+    echo "[verify-inet]: User selected Exit, exit status was $?."
+    installerexit
 fi
 
 packages
 exitstatus=$?
-[[ $exitstatus = 1 ]] && whiptail --title "Exit" --msgbox "You have exited the installer. Click OK to close this window." 8 78 && exit 1
+[[ $exitstatus = 1 ]] && installerexit
 
 whiptail --title "Finished" --msgbox "Installer has finished. Click OK exit." 8 78
